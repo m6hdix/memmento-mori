febfdd83ea13962d5cacabdde16e37df5d89b7c8
m6hdix <m6hdixx@gmail.com>
2024-11-08 16:35:06 +0330

    fix

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,16 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
+const prisma = global.prisma || new PrismaClient();
 
-if (typeof window === "undefined") {
-  if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient();
-  } else {
-    if (!global.prisma) {
-      global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
-  }
+if (process.env.NODE_ENV === "development") {
+  global.prisma = prisma;
 }
 
-export { prisma };
+export default prisma;
